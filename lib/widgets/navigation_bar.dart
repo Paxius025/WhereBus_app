7b565dbb19351f7958964a29e91c8e4b64d2b3c9
pantong <pantongbk@gmail.com>
2024-10-08 19:38:40 +0700

    unremove ontap

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -80,6 +80,7 @@ class _NavigationBarWidgetState extends State<NavigationBarWidget> {
       currentIndex: _selectedIndex,
       selectedItemColor: const Color(0xFF40534C), // สีเมื่อถูกเลือก
       unselectedItemColor: const Color(0xFF40534C), // สีเมื่อไม่ถูกเลือก
+      onTap: _onItemTapped,
     );
   }
 }
